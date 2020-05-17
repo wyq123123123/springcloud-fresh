e17deb4a3e2234e3d0924eb2c75d6cc89ffc549b
wyq123123123 <2860308561@qq.com>
2020-05-17 14:12:03 +0800

    config的服务器

---
+++ springcloud-fresh-config-server/src/main/java/com/yc/fresh/config/server/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.yc.fresh.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer // 配置文件服务器，从配种中心读取配置文件的
+@EnableEurekaClient
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
